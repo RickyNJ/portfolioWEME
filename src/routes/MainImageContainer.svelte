78003bde664dbe3c671f6e97b6bab0f5550b0c93
Ricky Nijsten <rickynijsten@gmail.com>
2023-11-27 18:57:15 +0100

    added pictures

---
+++ src/routes/MainImageContainer.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+     export let imageid: string
+    $: imageUrl = "D:\Code\portfolioWEME\src\resources\pictures\weme"+ imageid + ".jpg";
+</script>
+
+<div id="img-container">
+    <img src={imageUrl} alt="person"/>
+</div>
+
+<style>
+
+</style>
